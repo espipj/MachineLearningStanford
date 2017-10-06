7fd70ede16f493634fd476e5839107f1ccfd50a5
Pablo Espinosa <espipj@usal.es>
2017-10-06 16:43:08 +0200

    sigmoid function completed

--- ex2/sigmoid.m
+++ ex2/sigmoid.m
@@ -9,8 +9,8 @@ g = zeros(size(z));
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
 
-
-
+div= 1+ exp(-z);
+g=1./div;
 
 
 % =============================================================
