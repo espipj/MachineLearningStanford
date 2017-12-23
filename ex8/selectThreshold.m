33e77f3d952757e57f9f7a12927dc69b74f59d3e
espipj <espipj@gmail.com>
2017-12-23 19:53:19 +0100

    final commit

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -24,18 +24,17 @@ for epsilon = min(pval):stepsize:max(pval)
     %       of 0's and 1's of the outlier predictions
 
 
+	predicciones = (pval < epsilon);
+    
+    fp = sum((predicciones == 1) & (yval == 0));
+    fn = sum((predicciones == 0) & (yval == 1));
+    tp = sum((predicciones == 1) & (yval == 1));
 
+    prec = tp / (tp + fp);
+    rec = tp / (tp + fn);
 
+    F1 = 2 * prec * rec / (prec + rec);
 
-
-
-
-
-
-
-
-
-    % =============================================================
 
     if F1 > bestF1
        bestF1 = F1;
